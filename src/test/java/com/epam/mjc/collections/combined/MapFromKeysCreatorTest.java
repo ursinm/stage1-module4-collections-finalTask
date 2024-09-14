df4a84e79853c4db847d63e91234be7981ec9fa8
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 04:04:01 +0300

    Update MapFromKeysCreatorTest.java

--- src/test/java/com/epam/mjc/collections/combined/MapFromKeysCreatorTest.java
+++ src/test/java/com/epam/mjc/collections/combined/MapFromKeysCreatorTest.java
@@ -11,12 +11,15 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MapFromKeysCreatorTest {
+
     public static Stream<Arguments> testCases() {
         return Stream.of(
+                // Пустая карта
                 Arguments.of("EmptySourceMap",
                         Map.of(),
                         Map.of()),
 
+                // Карта с разными длинами ключей
                 Arguments.of("FilledSourceMap",
                         Map.of("Monday", 1,
                                 "Tuesday", 2,
@@ -26,12 +29,33 @@ public class MapFromKeysCreatorTest {
                         Map.of(6, Set.of("Monday", "Friday"),
                                 7, Set.of("Tuesday"),
                                 8, Set.of("Thursday"),
-                                9, Set.of("Wednesday"))));
+                                9, Set.of("Wednesday"))),
+
+                // Карта с ключами одинаковой длины
+                Arguments.of("SameLengthKeys",
+                        Map.of("cat", 1,
+                                "dog", 2,
+                                "bat", 3),
+                        Map.of(3, Set.of("cat", "dog", "bat"))),
+
+                // Карта с одним значением
+                Arguments.of("SingleEntry",
+                        Map.of("apple", 1),
+                        Map.of(5, Set.of("apple"))),
+
+                // Карта с ключами, содержащими пробелы и специальные символы
+                Arguments.of("KeysWithSpacesAndSymbols",
+                        Map.of("hello world", 1,
+                                "foo-bar", 2,
+                                "space key", 3),
+                        Map.of(11, Set.of("hello world", "space key"),
+                                7, Set.of("foo-bar")))
+        );
     }
 
     @ParameterizedTest(name = "createMap_{0}_Test")
     @MethodSource(value = "testCases")
-    void createMap(String name, Map<String, Integer> sourceMap, Map<Integer, Set<String>> expectedMapFromKeys) {
+    void createMapTest(String name, Map<String, Integer> sourceMap, Map<Integer, Set<String>> expectedMapFromKeys) {
 
         MapFromKeysCreator creator = new MapFromKeysCreator();
         Map<Integer, Set<String>> actualMapFromKeys = creator.createMap(sourceMap);
