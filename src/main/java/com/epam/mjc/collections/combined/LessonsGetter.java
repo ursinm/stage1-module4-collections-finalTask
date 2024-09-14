b8363efa8865a6d4bae42d508b298919ee5eed7f
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 03:59:59 +0300

    Update LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,19 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> uniqueLessons = new HashSet<>();
+        
+        // Проходим по каждому дню недели и добавляем уроки в множество
+        for (List<String> lessons : timetable.values()) {
+            uniqueLessons.addAll(lessons);
+        }
+        
+        return uniqueLessons;
     }
 }
